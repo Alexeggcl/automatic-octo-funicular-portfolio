4f587cc436eb9a604681164e336647580d59d1b4
lockcl <agonza43@ucm.es>
2023-10-06 22:49:09 +0200

    estilos de la timeline

--- components/experience.tsx
+++ components/experience.tsx
@@ -16,25 +16,27 @@ export default function Experience() {
     return (
         <section id="experience" ref={ref} className="scroll-mt-28 mb-28 sm:mb-40">
             <SectionHeading>My experience</SectionHeading>
-            <VerticalTimeline lineColor="">
+            <VerticalTimeline lineColor="#ffd864">
                 {experiencesData.map((item, index) => ( 
                     <React.Fragment key={index}>
                         <VerticalTimelineElement
                             contentStyle={{
                                 background: "rgba(255, 255, 255, 0.05)",
                                 boxShadow: "none",
-                                border: "1px solid rgba(0, 0, 0, 0.05)",
+                                border: "1px solid #ffd864",
                                 textAlign: "left",
                                 padding: "1.3rem 2rem",
                             }}
                             contentArrowStyle={{
-                                borderRight: "0.4rem solid rgba(255, 255, 255, 0.5)",
+                                borderRight: "0.4rem solid #ffd864",
                             }}  
                             date={item.date}
                             icon={item.icon}
                             iconStyle={{
-                                background: "rgba(255, 255, 255, 0.15)",
+                                background: "#111827",
+                                color: "#ffd864",
                                 fontSize: "1.5rem",
+                                boxShadow: "0 0 0 4px #ffd864, inset 0 2px 0 rgba(0,0,0,.08), 0 3px 0 4px rgba(0,0,0,.05)"
                             }}
                         >
                             <h3 className="font-semibold capitalize">{item.title}</h3>
