0b453daf6419f3689645844d93abd4b6c1d5a3cb
lockcl <agonza43@ucm.es>
2023-10-08 11:53:59 +0200

    Cambiados porcentajes por expresiones

--- components/skills.tsx
+++ components/skills.tsx
@@ -47,7 +47,7 @@ export default function Skills() {
             <ul className="flex flex-wrap justify-center gap-2 text-lg text-gray-800">
                 {skillsData.map((skill, index) => (
                     <motion.li
-                        className="border rounded-xl border-[#ffd864] px-5 py-3 bg-white/10 text-white/80 flex items-center gap-2 group transition-opacity duration-300 ease-in-out hover:opacity-0 overflow-hidden"
+                        className="border rounded-xl border-[#ffd864] px-5 py-3 bg-white/10 text-white/80 flex items-center gap-2 group transition-opacity duration-300 ease-in-out hover:opacity-0 overflow-hidden min-w-[120px]"
                         key={index}
                         variants={fadeInAnimationVariants}
                         initial="initial"
@@ -64,7 +64,7 @@ export default function Skills() {
                             <motion.div animate={controlsArray[index]}>
                                 <motion.span >{skill.name}</motion.span>
                                 <motion.span className="absolute top-[50px] left-0 w-full h-full opacity-100" >
-                                    {skill.pctg + "%"}
+                                    {skill.pctg}
                                 </motion.span>
                             </motion.div>
                         </div>
