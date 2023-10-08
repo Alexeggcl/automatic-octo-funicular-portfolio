7f9396bf64c072b1d81676663259fcf6f0b1e863
lockcl <agonza43@ucm.es>
2023-10-08 11:56:51 +0200

    Fix error vercel?

--- components/skills.tsx
+++ components/skills.tsx
@@ -1,6 +1,6 @@
 "use client";
 
-import React from "react";
+import React, { useEffect } from "react";
 import SectionHeading from "./section-heading";
 import { skillsData } from "@/lib/data";
 import { useSectionInView } from "@/lib/hooks";
