3354a7b910ad3ea24aa6de37a5c2bb2809f56a0e
lockcl <agonza43@ucm.es>
2023-10-08 11:41:59 +0200

    Skill porcentaje

--- components/skills.tsx
+++ components/skills.tsx
@@ -4,8 +4,7 @@ import React from "react";
 import SectionHeading from "./section-heading";
 import { skillsData } from "@/lib/data";
 import { useSectionInView } from "@/lib/hooks";
-import { motion } from "framer-motion";
-import { FaGithubSquare } from "react-icons/fa";
+import { motion, useAnimation } from "framer-motion";
 
 const fadeInAnimationVariants = {
     initial: {
@@ -23,6 +22,20 @@ const fadeInAnimationVariants = {
 
 export default function Skills() {
     const { ref } = useSectionInView("Skills");
+    const controlsArray = skillsData.map(() => useAnimation());
+
+    const handleHoverStart = (index: number) => {
+        controlsArray[index].start({
+            translateY: -50,
+        });
+    };
+
+    const handleHoverEnd = (index: number) => {
+        controlsArray[index].start({
+            translateY: 0,
+        });
+    };
+
 
     return (
         <section
@@ -34,7 +47,7 @@ export default function Skills() {
             <ul className="flex flex-wrap justify-center gap-2 text-lg text-gray-800">
                 {skillsData.map((skill, index) => (
                     <motion.li
-                        className="border rounded-xl border-[#ffd864] px-5 py-3 bg-white/10 text-white/80 flex items-center gap-2"
+                        className="border rounded-xl border-[#ffd864] px-5 py-3 bg-white/10 text-white/80 flex items-center gap-2 group transition-opacity duration-300 ease-in-out hover:opacity-0 overflow-hidden"
                         key={index}
                         variants={fadeInAnimationVariants}
                         initial="initial"
@@ -43,9 +56,18 @@ export default function Skills() {
                             once: true,
                         }}
                         custom={index}
+                        onHoverStart={() => handleHoverStart(index)}
+                        onHoverEnd={() => handleHoverEnd(index)}
                     >
-                        {skill.icon && <skill.icon size={24} style={{color: '#ffd864'}}/>}
-                        {skill.name}
+                        {skill.icon && <skill.icon size={24} style={{ color: '#ffd864' }} />}
+                        <div className="text-center grid place-items-center relative">
+                            <motion.div animate={controlsArray[index]}>
+                                <motion.span >{skill.name}</motion.span>
+                                <motion.span className="absolute top-[50px] left-0 w-full h-full opacity-100" >
+                                    {skill.pctg + "%"}
+                                </motion.span>
+                            </motion.div>
+                        </div>
                     </motion.li>
                 ))}
             </ul>
