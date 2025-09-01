d88183f9f0f05fac622cc5ff7770d4c8242019a3
Alexeggcl <alejandroeggcl@gmail.com>
2025-09-01 20:10:00 +0200

    chore: update dependencies to latest versions

---
+++ app/HomeClient.tsx
@@ -0,0 +1,27 @@
+"use client";
+import dynamic from "next/dynamic";
+import I18nLayout from "./i18n-layout";
+
+const About = dynamic(() => import("@/components/about"), { ssr: false });
+const Experience = dynamic(() => import("@/components/experience"), { ssr: false });
+const Footer = dynamic(() => import("@/components/footer"), { ssr: false });
+const Intro = dynamic(() => import("@/components/intro"), { ssr: false });
+const Projects = dynamic(() => import("@/components/projects"), { ssr: false });
+const SectionDivider = dynamic(() => import("@/components/section-divider"), { ssr: false });
+const Skills = dynamic(() => import("@/components/skills"), { ssr: false });
+
+export default function HomeClient() {
+    return (
+        <I18nLayout>
+            <main className="flex flex-col items-center px-4">
+                <Intro />
+                <SectionDivider />
+                <About />
+                <Skills />
+                <Experience />
+                <Projects />
+                <Footer />
+            </main>
+        </I18nLayout>
+    );
+}
--- app/page.tsx
+++ app/page.tsx
@@ -1,26 +1,5 @@
-import dynamic from "next/dynamic";
-import I18nLayout from "./i18n-layout";
-
-const About = dynamic(() => import("@/components/about"), { ssr: false });
-const Experience = dynamic(() => import("@/components/experience"), { ssr: false });
-const Footer = dynamic(() => import("@/components/footer"), { ssr: false });
-const Intro = dynamic(() => import("@/components/intro"), { ssr: false });
-const Projects = dynamic(() => import("@/components/projects"), { ssr: false });
-const SectionDivider = dynamic(() => import("@/components/section-divider"), { ssr: false });
-const Skills = dynamic(() => import("@/components/skills"), { ssr: false });
+import HomeClient from "./HomeClient";
 
 export default function Home() {
-  return (
-    <I18nLayout>
-      <main className="flex flex-col items-center px-4">
-        <Intro />
-        <SectionDivider />
-        <About />
-        <Projects />
-        <Skills />
-        <Experience />
-        <Footer />
-      </main>
-    </I18nLayout>
-  )
+  return <HomeClient />;
 }
